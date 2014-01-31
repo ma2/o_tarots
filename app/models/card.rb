d0e873dd99299015ce6071948d6a67f6990adb10
keiichi matsunaga <keiichi.2004.matsunaga@gmail.com>
2014-02-01 00:56:01 +0900

    * モバイル対応: タロットの一覧表示をテーブルからdivに変更

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,4 +2,10 @@ class Card < ActiveRecord::Base
   belongs_to :tarot
   validates_presence_of :name, :text
   acts_as_list scope: :tarot, column: :number, top_of_list: 0
+
+  # ローマ数字でnumberを返す
+  def roman
+    %w(0 I II III IV V VI VII VIII IX X XI XII XIII XIV XV XVI XVII XVIII XIX XX XXI)[number]
+  end
+
 end
