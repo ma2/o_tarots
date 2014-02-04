c1e6af7dc4df0d7a404f96cdfbda823887fbdd84
keiichi matsunaga <keiichi.2004.matsunaga@gmail.com>
2014-02-04 22:54:03 +0900

    * 機能追加: カード番号のローマ数字変換を30まで対応

--- app/models/card.rb
+++ app/models/card.rb
@@ -4,8 +4,9 @@ class Card < ActiveRecord::Base
   acts_as_list scope: :tarot, column: :number, top_of_list: 0
 
   # ローマ数字でnumberを返す
+  # 0～30まで対応
   def roman
-    %w(0 I II III IV V VI VII VIII IX X XI XII XIII XIV XV XVI XVII XVIII XIX XX XXI)[number]
+    %w(0 I II III IV V VI VII VIII IX X XI XII XIII XIV XV XVI XVII XVIII XIX XX XXI XXII XXIII XXIV XXV XXVI XXVII XXVIII XXIX XXX)[number]
   end
 
 end
